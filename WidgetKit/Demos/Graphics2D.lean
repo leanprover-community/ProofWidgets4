477f31701f5786704e0e0191bb238bbdffa36fb6
lecopivo <skrivantomas@seznam.cz>
2022-11-16 17:35:10 +0100

    generic svg structure

--- WidgetKit/Demos/Graphics2D.lean
+++ WidgetKit/Demos/Graphics2D.lean
@@ -4,23 +4,27 @@ import WidgetKit.Json
 open Lean.Widget.Jsx
 open Lean Widget
 
+
+namespace Svg
+
 structure Color where
   (r := 0.0)
   (g := 0.0)
   (b := 0.0)
-
+deriving ToJson, FromJson
 
 def Color.toRGB (c : Color) : String := s!"rgb({255*c.r}, {255*c.g}, {255*c.b})"
 
-
 structure Point where
   (x y : Float)
-deriving Inhabited
+deriving Inhabited, ToJson, FromJson
+
 
 structure Frame where
   (min : Point)
   (xSize : Float)
   (width height : Nat)
+deriving ToJson, FromJson
 
 def Frame.ySize (frame : Frame) : Float := frame.height.toFloat * (frame.xSize / frame.width.toFloat)
 
@@ -38,92 +42,106 @@ def Point.toPixels (p : Point) (frame : Frame) : Nat × Nat :=
 
 
 inductive Size where
-| zero : Size
 | pixels   (size : Nat)   : Size
 | absolute (size : Float) : Size
+deriving ToJson, FromJson
 
 def Size.toPixels (s : Size) (frame : Frame) : Nat :=
   match s with
-  | .zero => 0
   | .pixels   x => x
   | .absolute x => x * (frame.width.toFloat / frame.xSize) |>.toUInt64.toNat
 
 
-structure Edge where
-  (src trg : Point)
-  (width : Size)
-  (color : Color)
-
-def Edge.toSvgHtml (edge : Edge) (frame : Frame) : Html := 
-  let (x1,y1) := edge.src.toPixels frame
-  let (x2,y2) := edge.trg.toPixels frame
-  let strokeWidth := edge.width.toPixels frame;
-  <line x1={x1} y1={y1} x2={x2} y2={y2} stroke={edge.color.toRGB} strokeWidth={strokeWidth} /> 
-
-
-structure Circle where
-  (center : Point)
-  (radius : Size)
-  (color : Color)
-
-def Circle.toSvgHtml (circle : Circle) (frame : Frame) : Html :=
-  let (cx,cy) := circle.center.toPixels frame
-  let r := circle.radius.toPixels frame;
-  <circle cx={cx} cy={cy} r={r} fill={circle.color.toRGB} />
-
-
-structure Polygon where
-  (points : Array Point)
-  (color : Color)
-
-def Polygon.toSvgHtml (polygon : Polygon) (frame : Frame) : Html :=
-  let pixelPoint (p : Point) : String := 
-    let (x,y) := p.toPixels frame
-    s!"{x},{y}"
-  let points : String := polygon.points[1:].foldl (init := pixelPoint polygon.points[0]!) 
-    (λ s p => s ++ " " ++ pixelPoint p);
-
-  <polygon fillRule="nonzero" fill={polygon.color.toRGB} points={points} />
-
-inductive SvgShape where
+inductive Shape where
 | line     (src trg : Point)
 | circle   (center : Point) (radius : Size)
 | polyline (points : Array Point)
 | polygon  (points : Array Point)
+deriving ToJson, FromJson
 
-/- def SvgShape.to : SvgShape → String × Array (String)
-| .line src trg width => sorry
-| .circle center radius => sorry
-| .polyline points width 
- -/
+def Shape.toHtmlData (frame : Frame) : Shape → String × Array (String × Json)
+| .line src trg => 
+  let (x1,y1) := src.toPixels frame
+  let (x2,y2) := trg.toPixels frame
+  ("line", #[("x1", x1), ("y1", y1), ("x2", x2), ("y2", y2)])
+| .circle center radius => 
+  let (cx,cy) := center.toPixels frame
+  let r := radius.toPixels frame
+  ("circle", #[("cx", cx), ("cy", cy), ("r", r)])
+| .polyline points => 
+  let pts := points 
+      |>.map (λ p => let (x,y) := p.toPixels frame; s!"{x},{y}")
+      |>.foldl (init := "") (λ s p => s ++ " " ++ p)
+  ("path", #[("points", pts)])
+| .polygon points => 
+  let pts := points 
+      |>.map (λ p => let (x,y) := p.toPixels frame; s!"{x},{y}")
+      |>.foldl (init := "") (λ s p => s ++ " " ++ p)
+  ("polygon", #[("fillRule", "nonzero"), ("points", pts)])
+
  
-structure SvgElement where
-  shape : SvgShape
-  strokeColor : Color
-  strokeWidth : Size
-  id : String
+structure Element where
+  shape : Shape
+  strokeColor := (none : Option Color)
+  strokeWidth := (none : Option Size)
+  fillColor   := (none : Option Color)
+  id          := (none : Option String)
+deriving ToJson, FromJson
+
+def Element.toHtml (frame : Frame) (e : Element) : Html := Id.run do
+  let mut (tag, args) := e.shape.toHtmlData frame
+
+  if let .some color ← e.strokeColor then
+    args := args.push ("stroke", color.toRGB)
+  if let .some width ← e.strokeWidth then
+    args := args.push ("strokeWidth", width.toPixels frame)
+  if let .some color ← e.fillColor then
+    args := args.push ("fill", color.toRGB)
+  if let .some id ← e.id then
+    args := args.push ("click", id)
+
+  return .element tag args #[]
+
+
+end Svg
+
+
+def mkIdToIdx (elements : Array Svg.Element) : HashMap String (Fin elements.size) := 
+  let idToIdx := (elements
+    |>.mapIdx (λ idx el => (idx,el))) -- zip with index
+    |>.filterMap (λ (idx,el) => el.id.map (λ id => (id,idx))) -- keep only elements with specified id
+    |>.toList 
+    |> HashMap.ofList
+  idToIdx
+
+
+structure Svg where
+  elements : Array Svg.Element
+  frame : Svg.Frame
+  idToIdx := mkIdToIdx elements 
+
+
+def Svg.toHtml (svg : Svg) : Html := 
+  .element "svg" 
+           #[("xmlns", "http://www.w3.org/2000/svg"), 
+             ("version", "1.1"), 
+             ("width", svg.frame.width), 
+             ("height", svg.frame.height)] 
+           (svg.elements.map λ e => e.toHtml svg.frame)
   
+instance : GetElem Svg Nat Svg.Element (λ svg idx => idx < svg.elements.size) where
+  getElem svg i h := svg.elements[i]
 
--- structure Triangle where
---   (a b c : Point)
---   (color : Color)
-
-structure GeometryData where
-  edges     := (#[] : Array Edge)
-  circles   := (#[] : Array Circle)
-  polygons  := (#[] : Array Polygon)
-  -- triangles : Array Triangle
-
-def GeometryData.toSvgHtml (data : GeometryData) (frame : Frame) : Html := Id.run do
-
-  let items := (data.edges.map   λ x => x.toSvgHtml frame) |>.append
-               (data.circles.map λ x => x.toSvgHtml frame)
-
-  return .element "svg" #[("xmlns", "http://www.w3.org/2000/svg"), ("version", "1.1"), ("width", frame.width), ("height", frame.height)] items
-
+instance : GetElem Svg String (Option Svg.Element) (λ _ _ => True) where
+  getElem svg id _ := 
+    match svg.idToIdx[id] with
+    | some idx => svg.elements[idx]
+    | none => none
 
 
 section Example
+
+open Svg
 
 private def frame : Frame where
   min    := ⟨-1,-1⟩
@@ -132,15 +150,17 @@ private def frame : Frame where
   height := 400
 
 
-private def data : GeometryData where
-  edges   := #[{ src := ⟨0, 0⟩, trg := ⟨0, 1⟩, width := .pixels 2, color := { r := 1, g := 0, b := 0 }},
-               { src := ⟨0, 1⟩, trg := ⟨1, 0⟩, width := .pixels 2, color := { r := 0, g := 1, b := 0 }},
-               { src := ⟨1, 0⟩, trg := ⟨0, 0⟩, width := .pixels 2, color := { r := 0, g := 0, b := 1 }}]
-  circles := #[{ center := ⟨0,0⟩, radius := .pixels 5, color := ⟨1,1,0⟩ },
-               { center := ⟨1,0⟩, radius := .absolute 0.1, color := ⟨1,0,1⟩ },
-               { center := ⟨0,1⟩, radius := .pixels 5, color := ⟨0,1,1⟩ }]
-  
--- #eval toJson (data.toSvgHtml frame)
-#html data.toSvgHtml frame
+private def svg : Svg := 
+  { elements := 
+      #[{ shape := .line ⟨0,0⟩ ⟨1,0⟩, strokeWidth := some (.pixels 2), strokeColor := some ⟨1,0,0⟩, id := some "line1"},
+        { shape := .line ⟨1,0⟩ ⟨0,1⟩, strokeWidth := some (.pixels 2), strokeColor := some ⟨0,1,0⟩, id := some "line2"},
+        { shape := .line ⟨0,1⟩ ⟨0,0⟩, strokeWidth := some (.pixels 2), strokeColor := some ⟨0,0,1⟩, id := some "line3"},
+        { shape := .circle ⟨0,0⟩ (.absolute 0.1) , strokeWidth := some (.pixels 2), strokeColor := some ⟨0,0,0⟩, fillColor := some ⟨0,1,1⟩, id := some "circle1"},
+        { shape := .circle ⟨1,0⟩ (.absolute 0.1) , strokeWidth := some (.pixels 2), strokeColor := some ⟨0,0,0⟩, fillColor := some ⟨1,0,1⟩, id := some "circle2"},
+        { shape := .circle ⟨0,1⟩ (.absolute 0.1) , strokeWidth := some (.pixels 2), strokeColor := some ⟨0,0,0⟩, fillColor := some ⟨1,1,0⟩, id := some "circle3"}
+             ],
+    frame := frame }
+
+#eval toJson svg.toHtml
 
 end Example
