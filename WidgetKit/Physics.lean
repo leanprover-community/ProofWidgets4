6ac03142190f8b2eb5a74ddfb2680c567d86f90b
lecopivo <skrivantomas@seznam.cz>
2022-11-16 17:55:40 +0100

    update of Physics.lean with new svg strucuture

--- WidgetKit/Physics.lean
+++ WidgetKit/Physics.lean
@@ -5,15 +5,22 @@ import WidgetKit.Demos.Graphics2D
 open Lean Widget Jsx
 
 
-structure GeometryState where
-  geom : GeometryData
+structure State where
+  points : Array (Float × Float)
   t : Float := 0
   v := 0.0002
   deriving ToJson, FromJson
 
-def GeometryState.init : GeometryState := {
-  geom := initialData,
-  t := 0
+
+def State.toSvg (s : State) : Svg :=
+  let elements : Array Svg.Element := 
+    s.points 
+      |>.mapIdx (λ idx p => { shape := .circle ⟨p.1,p.2⟩ (.absolute 0.1), id := some s!"circle{idx}"} )
+  { elements := elements,
+    frame := {min := ⟨-1,-1⟩, xSize := 2, width := 400, height := 400} }
+
+def GeometryState.init : State := {
+  points := #[(-0.5,-0.5),(0.5,-0.5),(0,0.5)]
 }
 
 inductive ActionKind where
@@ -30,12 +37,12 @@ structure Action where
 structure UpdatePhysicsParams where
   elapsed : Float
   actions : Array Action
-  state : GeometryState
+  state : State
   deriving ToJson, FromJson
 
 structure UpdatePhysicsResult where
   html : Widget.Html
-  state : GeometryState
+  state : State
   /-- Approximate number of milliseconds to wait before calling again. -/
   callbackTime : Option Float := some 100
   deriving ToJson, FromJson
@@ -49,11 +56,16 @@ def updatePhysics (params : UpdatePhysicsParams ) : RequestM (RequestTask Update
   for action in params.actions do
     if action.kind == ActionKind.click then
       v := v * 2
-  let s2 := params.state.geom.rotate (δt * v)
 
+  -- update point position
+  let s2 : Array (Float × Float) := 
+    let θ := δt * v 
+    let c := Float.cos θ
+    let s := Float.sin θ
+    params.state.points.map λ p => (c*p.1 - s*p.2, s*p.1 + c*p.2)
 
-  let newState : GeometryState := {geom := s2, t := params.elapsed, v := v}
-  let svg := GeometryData.toSvgHtml newState.geom frame
+  let newState : State := {points := s2, t := params.elapsed, v := v}
+  let svg := newState.toSvg.toHtml
   return RequestTask.pure $ {
     html := <div>
       <div>
