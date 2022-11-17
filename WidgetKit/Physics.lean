dd0415020332a3906e067f882993bb4528bd55c2
lecopivo <skrivantomas@seznam.cz>
2022-11-17 10:46:08 +0100

    chasing spherese demo

--- WidgetKit/Physics.lean
+++ WidgetKit/Physics.lean
@@ -5,22 +5,34 @@ import WidgetKit.Demos.Graphics2D
 open Lean Widget Jsx
 
 
+
 structure State where
-  points : Array (Float × Float)
+  points : Array ((Float × Float) × (Float × Float) × (Float × Float))
   t : Float := 0
-  v := 0.0002
   deriving ToJson, FromJson
 
 
 def State.toSvg (s : State) : Svg :=
   let elements : Array Svg.Element := 
-    s.points 
-      |>.mapIdx (λ idx p => { shape := .circle ⟨p.1,p.2⟩ (.absolute 0.1), id := some s!"circle{idx}"} )
+    #[] 
+      -- |>.append <| s.points.mapIdx
+      --   (λ idx ((x,y),(vx,vy),(rx,ry)) => 
+      --     { shape := .line ⟨x,y⟩ ⟨rx,ry⟩, 
+      --       strokeWidth := some (.pixels 2), 
+      --       strokeColor := some ⟨1,1,1⟩, 
+      --       id := some s!"line{idx}"} )
+
+      |>.append <| s.points.mapIdx
+        (λ idx ((x,y),(vx,vy),(rx,ry)) => 
+          { shape := .circle ⟨x,y⟩ (.absolute 0.1), 
+            fillColor := let speed := 100*Float.sqrt (vx*vx + vy*vy); some ⟨1-speed, speed, 0⟩, 
+            id := some s!"circle{idx}"} )
+
   { elements := elements,
     frame := {min := ⟨-1,-1⟩, xSize := 2, width := 400, height := 400} }
 
 def GeometryState.init : State := {
-  points := #[(-0.5,-0.5),(0.5,-0.5),(0,0.5)]
+  points := #[((-0.5,0.0),(0,0), (-0.5,0)),((0.5,0),(0,0), (0.5,0))],
 }
 
 inductive ActionKind where
@@ -52,25 +64,37 @@ open Server RequestM in
 @[server_rpc_method]
 def updatePhysics (params : UpdatePhysicsParams ) : RequestM (RequestTask UpdatePhysicsResult) := do
   let δt := (params.elapsed - params.state.t)
-  let mut v := params.state.v
+
+  let mut points := params.state.points
+
+  let svg := params.state.toSvg
+
   for action in params.actions do
-    if action.kind == ActionKind.click then
-      v := v * 2
+    match fromJson? (α := String) action.value with
+    | .error _ => continue
+    | .ok id => 
+      if let .some idx := svg.idToIdx[id] then
+        points := points.modify idx.1
+          -- λ ((x,y),(vx,vy),(rx,ry)) => ((x,y), (vx+(Float.cos (1000*params.state.t)) * 0.01, vy + (Float.sin (1000*params.state.t)) * 0.01), (rx,ry)) 
+          λ ((x,y),(vx,vy),(rx,ry)) => ((x + 0.1*(Float.cos (1000*params.state.t)),y + 0.1 * (Float.sin (1000*params.state.t))), (vx, vy), (rx,ry)) 
+
 
   -- update point position
-  let s2 : Array (Float × Float) := 
-    let θ := δt * v 
-    let c := Float.cos θ
-    let s := Float.sin θ
-    params.state.points.map λ p => (c*p.1 - s*p.2, s*p.1 + c*p.2)
-
-  let newState : State := {points := s2, t := params.elapsed, v := v}
-  let svg := newState.toSvg.toHtml
-  return RequestTask.pure $ {
+/-   points :=
+    let k := 0.0001
+    let β := Float.exp (-0.001*δt)
+    points.map λ ((x,y),(vx,vy), (rx,ry)) => 
+      let vx := β*(vx-k*δt*(x-rx))
+      let vy := β*(vy-k*δt*(y-ry))
+      ((x+δt*vx,y+δt*vy), (vx,vy), (rx,ry))
+ -/
+  let newState : State := {points := points, t := params.elapsed }
+    return RequestTask.pure $ {
     html := <div>
       <div>
-        {svg}
+        {newState.toSvg.toHtml}
       </div>
+
       {toString params.elapsed} {toString <| toJson <| params.actions}</div>,
     state := newState,
     callbackTime := some 10,
