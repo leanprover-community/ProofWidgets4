afed2077da6a9e87a3071097ef117c5f5bfe1201
E.W.Ayers <edward.ayers@outlook.com>
2022-11-18 14:46:37 +0000

    feat: css hovers

--- WidgetKit/Demos/Graphics2D.lean
+++ WidgetKit/Demos/Graphics2D.lean
@@ -98,7 +98,7 @@ def Element.toHtml (frame : Frame) (e : Element) : Html := Id.run do
   if let .some color ← e.fillColor then
     args := args.push ("fill", color.toRGB)
   if let .some id ← e.id then
-    args := args.push ("id", id)
+    args := args.push ("id", id) |>.push ("className", "pointer dim")
 
   return .element tag args #[]
 
