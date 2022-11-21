5740046a4301ec5c8aa952a7f3ca520f237f88cb
lecopivo <skrivantomas@seznam.cz>
2022-11-21 12:55:02 +0100

    chore: interactive svg files reorganization

---
+++ WidgetKit/Demos/InteractiveSvg.lean
@@ -0,0 +1,63 @@
+import WidgetKit.InteractiveSvg
+
+open Lean.Widget.Jsx
+open Lean Widget
+
+open Svg
+
+abbrev State := Array (Float × Float)
+
+def isvg : InteractiveSvg State where
+  init := #[(-0.5, -0.5), (0.5, -0.5), (0.5, 0.5), (-0.5, 0.5)]
+
+  frame := 
+    { xmin := -1
+      ymin := -1
+      xSize := 2
+      width := 400
+      height := 400 }
+
+  update time Δt action mouseStart mouseEnd selected getData state :=
+    match getData Nat, mouseEnd with 
+    | some id, some p => state.set! id p.toAbsolute
+    | _, _ => state
+
+  render mouseStart mouseEnd state := 
+    { 
+      elements := 
+        let mousePointer := 
+          match mouseStart, mouseEnd with
+          | some s, some e => 
+            #[ 
+              Svg.circle e (.px 5) |>.setFill (1.,1.,1.),
+              Svg.line s e |>.setStroke (1.,1.,1.) (.px 2)
+            ]
+          | _, _ => #[]
+        let circles := (state.mapIdx fun idx (p : Float × Float) => 
+              Svg.circle p (.abs 0.2) |>.setFill (0.7,0.7,0.7) |>.setId s!"circle{idx}" |>.setData idx.1
+            )
+        mousePointer.append circles
+    }
+
+
+open Server RequestM in
+@[server_rpc_method]
+def updateSvg (params : UpdateParams State) : RequestM (RequestTask (UpdateResult State)) := isvg.serverRpcMethod params
+
+@[widget]
+def svgWidget : UserWidgetDefinition where
+  name := "Interactive SVG"
+  javascript := include_str ".." / ".." / "widget" / "dist" / "interactiveSvg.js"
+
+
+def init : UpdateResult State := {
+  html := <div>Init!!!</div>,
+  state := { state := isvg.init
+             time := 0
+             selected := none
+             mousePos := none
+             idToData := isvg.render none none isvg.init |>.idToDataList}
+}
+
+#widget svgWidget (toJson init)
+
