aa4bec8417532a2e763fe098cba37a2077b618a3
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-02-01 23:21:05 -0500

    feat: add Rubik's cube

---
+++ WidgetKit/Demos/Rubiks.lean
@@ -0,0 +1,16 @@
+import WidgetKit.Component.HtmlDisplay
+
+open Lean WidgetKit
+open scoped WidgetKit.Jsx
+
+structure RubiksProps where
+  seq : Array String := #[]
+  deriving ToJson, FromJson, Inhabited
+
+@[widget_module]
+def Rubiks : Component RubiksProps where
+  javascript := include_str ".." / ".." / "build" / "js" / "rubiks.js"
+
+def eg := #["L", "L", "D⁻¹", "U⁻¹", "L", "D", "D", "L", "U⁻¹", "R", "D", "F", "F", "D"]
+
+#html <Rubiks seq={eg} />
