87794c18c80f90c1acc80f0705e997af65ada7d2
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-04-12 23:52:23 -0400

    chore: update files

--- ProofWidgets.lean
+++ ProofWidgets.lean
@@ -1,9 +1,11 @@
 import ProofWidgets.Component.Basic
 import ProofWidgets.Component.HtmlDisplay
 import ProofWidgets.Component.InteractiveSvg
+import ProofWidgets.Component.Panel
 import ProofWidgets.Component.PenroseDiagram
+import ProofWidgets.Component.Recharts
+import ProofWidgets.Component.SelectionPanel
 import ProofWidgets.Data.Html
 import ProofWidgets.Data.Json
 import ProofWidgets.Data.Svg
 import ProofWidgets.Presentation.Expr
-import ProofWidgets.Presentation.Goal
