861bab112682165ada5719ed5818196059abfb95
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-07-05 08:43:02 -0700

    refactor: TSX and types in selectInsertPanel

--- widget/src/selectInsertPanel.tsx
+++ widget/src/selectInsertPanel.tsx
@@ -1,15 +1,14 @@
 import * as React from 'react'
-import { EditorContext, RpcContext, mapRpcError, useAsync, DocumentPosition, GoalsLocation } from '@leanprover/infoview'
-import { InteractiveGoal } from '@leanprover/infoview-api';
-import { Range } from 'vscode-languageserver-protocol'
-
-const e = React.createElement
+import { EditorContext, RpcContext, mapRpcError, useAsync, DocumentPosition, GoalsLocation }
+    from '@leanprover/infoview'
+import { InteractiveGoal } from '@leanprover/infoview-api'
+import { Position, Range, WorkspaceEdit } from 'vscode-languageserver-protocol'
 
 function findGoalForLocation(goals: InteractiveGoal[], loc: GoalsLocation) {
     for (const g of goals) {
         if (g.mvarId === loc.mvarId) return g
     }
-    throw new Error('could not find goal for location' + JSON.stringify(loc))
+    throw new Error(`could not find goal for location ${JSON.stringify(loc)}`)
 }
 
 interface SelectInsertParams {
@@ -22,29 +21,48 @@ interface SelectInsertParams {
     callback: string
 }
 
-export default function ({ pos, goals, selectedLocations, replaceRange, title, help, callback }: SelectInsertParams) {
+interface SelectInsertResponse {
+    content: string
+    edit: WorkspaceEdit
+    newCursorPos: Position
+}
+
+export default function({
+        pos, goals, selectedLocations, replaceRange, title, help, callback }: SelectInsertParams) {
     const rs = React.useContext(RpcContext)
     const ec = React.useContext(EditorContext)
-    const st: any = useAsync(async () => {
+    const st  = useAsync<SelectInsertResponse | undefined>(async () => {
         const g = findGoalForLocation(goals, selectedLocations[0])
-        return await rs.call(callback,
-            { cursorPos: pos, ctx: g.ctx, selectedLocations, replaceRange })
-    }, [goals, selectedLocations])
+        return await rs.call(callback, {
+            cursorPos: pos,
+            ctx: g.ctx,
+            selectedLocations,
+            replaceRange
+        })
+    }, [goals, selectedLocations, pos, replaceRange])
 
-    const onClick = () => void (async () => {
-        if (st.value) {
+    const onClick = async () => {
+        if (st.state === 'resolved' && st.value) {
             await ec.api.applyEdit(st.value.edit)
             await ec.revealPosition({ ...st.value.newCursorPos, uri: pos.uri })
         }
-    })()
+    }
 
-    const result = st.state === 'resolved' ? e('pre', { className: 'font-code pre-wrap' }, e('a', { onClick, className: 'link pointer dim', title: 'Apply suggestion' }, st.value.content))
-        : st.state === 'rejected' ? e('p', { style: { color: 'red' } }, mapRpcError(st.error).message)
-            : e('p', null, 'Loading...')
-    const inner = selectedLocations.length === 0 ? e('span', null, help) : result
+    const result =
+        st.state === 'resolved' ?
+            <pre className='font-code pre-wrap'>
+                <a onClick={onClick} className='link pointer dim' title='Apply suggestion'>
+                    {st.value && st.value.content}
+                </a>
+            </pre> :
+        st.state === 'rejected' ?
+            <p style={{color: 'red'}}>{mapRpcError(st.error).message}</p>
+        :
+            <p>Loading...'</p>
+    const inner = selectedLocations.length === 0 ? <span>{help}</span> : result
 
-    return e('details', { open: true }, [
-        e('summary', { className: 'mv2 pointer' }, title),
-        e('div', { className: 'ml1' }, inner)
-    ])
+    return <details open>
+        <summary className='mv2 pointer'>{title}</summary>
+        <div className='ml1'>{inner}</div>
+    </details>
 }
