cd10269b3855ef721aaeb5365c9097e0d87f01db
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2023-08-08 19:42:03 -0700

    feat: macro demo
    Co-authored-by: ART0 <18333981+0Art0@users.noreply.github.com>

---
+++ ProofWidgets/Demos/Macro.lean
@@ -0,0 +1,29 @@
+import ProofWidgets.Component.HtmlDisplay
+
+-- See the `Jsx.lean` demo for more about JSX.
+open scoped ProofWidgets.Jsx
+
+/-! # Widgets in macros
+
+Macros may expand to commands or terms that end up saving widget info,
+but the syntax to which the widget info is associated must be marked as `canonical`
+for the widget to be displayed.
+-/
+
+def Lean.SourceInfo.mkCanonical : SourceInfo → SourceInfo
+  | .synthetic s e _ => .synthetic s e true
+  | si => si
+
+def Lean.Syntax.mkInfoCanonical : Syntax → Syntax
+  | .missing => .missing
+  | .node i k a => .node i.mkCanonical k a
+  | .atom i v => .atom i.mkCanonical v
+  | .ident i r v p => .ident i.mkCanonical r v p
+
+macro "#browse " src:term : command => do
+  let s ← `(#html <iframe src={$src} width="100%" height="600px" />)
+  return ⟨s.raw.mkInfoCanonical⟩
+
+#browse "https://leanprover-community.github.io/"
+-- Do you like recursion?
+#browse "https://lean.math.hhu.de"
