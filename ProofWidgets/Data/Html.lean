1f044e3cf822d2990306db4cce27276f4ba46317
Wojciech Nawrocki <wjnawrocki+gh@protonmail.com>
2024-04-26 22:51:19 -0400

    docs: adjust

--- ProofWidgets/Data/Html.lean
+++ ProofWidgets/Data/Html.lean
@@ -128,19 +128,19 @@ partial def delabListLiteral {α} (elem : DelabM α) : DelabM (List α) := do
   | _ => failure
 
 
-/-- Delaborate the elements of a list literal separately, calling `elem` on each. -/
+/-- Delaborate the elements of an array literal separately, calling `elem` on each. -/
 partial def delabArrayLiteral {α} (elem : DelabM α) : DelabM (Array α) := do
   match_expr ← getExpr with
   | List.toArray _ _ => List.toArray <$> (withNaryArg 1 <| delabListLiteral elem)
   | _ => failure
 
-/-- A copy of `Delaborator.annotateTermLikeInfo` for other categories. -/
+/-- A copy of `Delaborator.annotateTermInfo` for other syntactic categories. -/
 def annotateTermLikeInfo (stx : TSyntax n) : DelabM (TSyntax n) := do
   let stx ← annotateCurPos ⟨stx⟩
   addTermInfo (← getPos) stx (← getExpr)
   pure ⟨stx⟩
 
-/-- A copy of `Delaborator.withAnnotateTermLikeInfo` for other categories. -/
+/-- A copy of `Delaborator.withAnnotateTermInfo` for other syntactic categories. -/
 def withAnnotateTermLikeInfo (d : DelabM (TSyntax n)) : DelabM (TSyntax n) := do
   let stx ← d
   annotateTermLikeInfo stx
