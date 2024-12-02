2b000e02d50394af68cfb4770a291113d94801b5
Kim Morrison <kim@tqft.net>
2024-12-02 14:02:11 +1100

    update

--- lakefile.lean
+++ lakefile.lean
@@ -6,7 +6,7 @@ package proofwidgets where
   buildArchive? := "ProofWidgets4.tar.gz"
   releaseRepo := "https://github.com/leanprover-community/ProofWidgets4"
 
-require "leanprover-community" / "batteries" @ git "v4.14.0"
+require "leanprover-community" / "batteries" @ git "v4.15.0-rc1"
 
 def npmCmd : String :=
   if Platform.isWindows then "npm.cmd" else "npm"
