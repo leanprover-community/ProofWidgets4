e6e5f29e079663dc36cf76c74dc4d6d672569a6d
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2025-08-16 19:10:26 -0400

    feat: canonicalize colors better
    Closes #132.

--- widget/src/penroseDisplay.tsx
+++ widget/src/penroseDisplay.tsx
@@ -28,26 +28,39 @@ export default function(props: DiagramData): JSX.Element {
             (acc, [nm, cwi]) => acc.set(nm, mkElt(cwi)),
             new Map<string, React.ReactNode>())
 
-    const cssColourToRgba = (col: string, alpha: number = 255) => {
-        if (col.startsWith('#')) {
-            const gps = col.match(/\w\w/g)
-            if (!gps) throw new Error(`cannot parse colour '${col}'`)
-            const [r, g, b] = gps.map(x => parseInt(x, 16))
-            return `rgba(${r}/255,${g}/255,${b}/255,${alpha}/255)`
-        } else throw new Error(`cannot parse colour '${col}'`)
+    /* A temporary element that we use to put colors in canonical form;
+     * there seems to be no way of doing this with a pure function.
+     * It must be in the DOM to pick up CSS styles. */
+    const colEl = document.createElement('div')
+    colEl.style.display = 'none'
+    document.body.appendChild(colEl)
+    const getCssColour = (col: string) => {
+        colEl.style.color = col
+        col = getComputedStyle(colEl).color
+        /* The value is guaranteed to be `rgb` or `rgba` in [0, 255].
+         * We convert this to `rgba` in [0, 1]. */
+        const gps = col.match(/[( ]\w+/g)
+        if (!gps) throw new Error(`cannot parse colour '${col}'`)
+        const cols = gps.map(x => parseInt(x.substring(1), 10))
+        let r, g, b, a
+        if (cols.length === 3) {
+            [r, g, b] = cols
+            a = '255'
+        } else if (cols.length === 4) {
+            [r, g, b, a] = cols
+        } else {
+            throw new Error(`unexpected channels in '${col}'`)
+        }
+        return `rgba(${r}/255,${g}/255,${b}/255,${a}/255)`
     }
 
-    const getCssColour = (col: string) =>
-        cssColourToRgba(
-            getComputedStyle(document.documentElement)
-                .getPropertyValue(col)
-        )
+    const colForeground = getCssColour('var(--vscode-editor-foreground)')
+    const colBackground = getCssColour('var(--vscode-editor-background)')
+    const colTooltipBackground = getCssColour('var(--vscode-editorHoverWidget-background)')
+    const colTooltipForeground = getCssColour('var(--vscode-editorHoverWidget-foreground)')
+    const colTooltipBorder = getCssColour('var(--vscode-editorHoverWidget-border)')
 
-    const colForeground = getCssColour('--vscode-editor-foreground')
-    const colBackground = getCssColour('--vscode-editor-background')
-    const colTooltipBackground = getCssColour('--vscode-editorHoverWidget-background')
-    const colTooltipForeground = getCssColour('--vscode-editorHoverWidget-foreground')
-    const colTooltipBorder = getCssColour('--vscode-editorHoverWidget-border')
+    document.body.removeChild(colEl)
 
     const sty = props.sty +
 `
