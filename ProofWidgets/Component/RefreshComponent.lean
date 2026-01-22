d9587eda60215463c1c009afc6df702b85ce01e1
Jovan Gerbscheid <jovan.gerbscheid@gmail.com>
2026-01-22 23:32:27 +0000

    add explanation of known limitations

--- ProofWidgets/Component/RefreshComponent.lean
+++ ProofWidgets/Component/RefreshComponent.lean
@@ -10,7 +10,7 @@ public import ProofWidgets.Util
 public import ProofWidgets.Component.Panel.Basic
 
 /-!
-## The `RefreshComponent` widget
+# The `RefreshComponent` widget
 
 This file defines `RefreshComponent`, which allows you to have an HTML widget that updates
 incrementally as more results are computed by a Lean computation.
@@ -22,6 +22,32 @@ To determine whether the widget is up to date, each computed HTML has an associa
 
 When the widget (re)loads, it first loads the current HTML from the ref, and then
 repeatedly awaits further HTML result.
+
+## Known limitations
+
+Cancellation is a bit hard to get right, and there are two limitations.
+
+1. When using a `RefreshComponent` that reacts to shift-clicking in the infoview,
+we want to cancel the computation whenever a new selection is made.
+We acomplish this in `mkCancelPanelWidget` by creating a reference to a cancel token,
+which we can reset every time a new selection is made.
+On the other hand, we would also like to cancel the computation if that part of the file gets
+reloaded. Unfortunately, the `CoreM` monad can only store up to 1 cancel token at a time.
+The result is that if you close the infoview while the widget is active, and then reload
+this part of the file, the widget computation keeps running without any way to stop it
+(except for restarting the file, or waiting it out)
+2. When using a `RefreshComponent` that comes directly from a command/tactic (no shift-clicking),
+then we pass the cancel token used by elaboration to the refresh component computation.
+Unfortunately there is an edge case where the cancel token gets set while the corresponding
+command does not get re-elaborated. In particular, if you have three command in a row, e.g.
+```
+#html countToTen
+#html countToTen
+#html countToTen
+```
+Then commenting out the third command will cancel the widget of the first command.
+It refreshes the second command, and doesn't affect commands before the first command - only
+the first command gets badly affected.
 -/
 
 public meta section
