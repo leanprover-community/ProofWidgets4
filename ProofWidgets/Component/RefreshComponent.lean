e7d85c8d680643c9994e46d8aff6d011e5f84679
Wojciech Nawrocki <wjnawrocki@protonmail.com>
2026-03-17 00:28:09 -0400

    refactor: handle cancellation when started via RPC method

--- ProofWidgets/Component/RefreshComponent.lean
+++ ProofWidgets/Component/RefreshComponent.lean
@@ -25,18 +25,9 @@ repeatedly awaits further HTML result.
 
 ## Known limitations
 
-Cancellation is a bit hard to get right, and there are two limitations.
+Cancellation is a bit hard to get right, and there is one limitation.
 
-1. When using a `RefreshComponent` that reacts to shift-clicking in the infoview,
-we want to cancel the computation whenever a new selection is made.
-We acomplish this in `mkCancelPanelWidget` by creating a reference to a cancel token,
-which we can reset every time a new selection is made.
-On the other hand, we would also like to cancel the computation if that part of the file gets
-reloaded. Unfortunately, the `CoreM` monad can only store up to 1 cancel token at a time.
-The result is that if you close the infoview while the widget is active, and then reload
-this part of the file, the widget computation keeps running without any way to stop it
-(except for restarting the file, or waiting it out)
-2. When using a `RefreshComponent` that comes directly from a command/tactic (no shift-clicking),
+1. When using a `RefreshComponent` that comes directly from a command/tactic (no shift-clicking),
 then we pass the cancel token used by elaboration to the refresh component computation.
 Unfortunately there is an edge case where the cancel token gets set while the corresponding
 command does not get re-elaborated. In particular, if you have three command in a row, e.g.
@@ -56,157 +47,128 @@ namespace ProofWidgets
 open Lean Server Widget Jsx
 namespace RefreshComponent
 
-/-- The result that is sent to the `RefreshComponent` after each query. -/
-structure VersionedHtml where
-  /-- The new HTML that will replace the current HTML. -/
-  html : Html
-  /-- The version number of the HTML. It is a count of how many HTMLs were created. -/
-  idx : Nat
-  deriving RpcEncodable, Nonempty
+/-- An HTML tree together with a task that may yield a new tree in the future.
+This is the server-side state of a `RefreshComponent` instance. -/
+structure UpdatableHtml where
+  /-- The HTML tree that the component should currently be displaying. -/
+  curr : Html
+  /-- A task that produces either the next HTML tree to display,
+  or `none` if the display will never be updated again.
+  It is implemented using `RefreshToken.promise.result?`,
+  but we don't store the `Promise` here
+  in order to make sure it can reach a refcount of 0. -/
+  next : Task (Option UpdatableHtml)
+  deriving TypeName, Nonempty
 
-/-- The `RefreshState` stores the incremental result of the HTML computation. -/
-structure RefreshState where
-  /-- The state that the widget should currently be in. -/
-  curr : VersionedHtml
-  /-- A task that returns the next state for the widget.
-  It is always implemented using `IO.Promise.result?`. -/
-  next : Task (Option VersionedHtml)
+/-- Wait for `state.next` and return the result.
+In case this result is `some nextState`, also return `nextState.curr`. -/
+@[server_rpc_method]
+def awaitNextHtml (state : WithRpcRef UpdatableHtml) :
+    RequestM (RequestTask (Option (Html × WithRpcRef UpdatableHtml))) :=
+  let { next, .. } := state.val
+  RequestM.mapTaskCheap ⟨next⟩ fun nextState? =>
+    nextState?.mapM fun nextState =>
+      return (nextState.curr, ← WithRpcRef.mk nextState)
 
-/-- A reference to a `RefreshState`. This is used to keep track of the refresh state. -/
-structure RefreshRef where
-  ref : IO.Ref RefreshState
-  deriving TypeName
+/-- Return `state.curr`. -/
+@[server_rpc_method]
+def getCurrHtml (state : WithRpcRef UpdatableHtml) : RequestM (RequestTask Html) :=
+  return .pure state.val.curr
 
-/-- The data used to call `awaitRefresh`, for updating the HTML display. -/
-structure AwaitRefreshParams where
-  /-- The reference to the `RefreshState`. -/
-  state : WithRpcRef RefreshRef
-  /-- The index of the HTML that is currently on display. -/
-  oldIdx : Nat
+deriving instance TypeName for IO.CancelToken
+
+structure Props where
+  /-- Initial state of the component. -/
+  state : WithRpcRef UpdatableHtml
+  /-- Will be cancelled whenever the widget is rendered with a new `state`, or unmounted,
+  or when the whole infoview is closed. -/
+  cancelTk : WithRpcRef IO.CancelToken
   deriving RpcEncodable
 
-
-/-- `awaitRefresh` is called through RPC to obtain the next HTML to display. -/
+/-- HACK: We cancel background threads driving `RefreshComponent`s when they are no longer needed.
+However, there is currently no good way to do this when the infoview closes,
+as in that case React effect cleanup functions are never executed.
+Instead, `RefreshComponent` calls this persistent monitor task during creation.
+The call is auto-cancelled when the infoview closes, setting `cancelTk`. -/
 @[server_rpc_method]
-def awaitRefresh (props : AwaitRefreshParams) : RequestM (RequestTask (Option VersionedHtml)) := do
-  let { curr, next } ← props.state.val.ref.get
-  -- If `props.oldIdx < curr.idx`, that means that the state has updated in the meantime.
-  -- So, returning `curr` will give a refresh.
-  -- If `props.oldIdx = curr.idx`, then we need to await `next` to get a refresh
-  if props.oldIdx = curr.idx then
-    return .mapCheap .ok ⟨next⟩
-  else
-    return .pure curr
-
-/--
-`getCurrState` is called through RPC whenever the widget reloads.
-This can be because the infoview was closed and reopened,
-or because a different expression was selected in the goal.
--/
-@[server_rpc_method]
-def getCurrState (ref : WithRpcRef RefreshRef) : RequestM (RequestTask VersionedHtml) := do
-  return .pure (← ref.val.ref.get).curr
+partial def monitor (p : Props) : RequestM (RequestTask Unit) :=
+  RequestM.asTask do
+    repeat do
+      IO.sleep 1000
+      if ← (← read).cancelTk.wasCancelledByCancelRequest then
+        p.cancelTk.val.set
+        return ()
 
 end RefreshComponent
 
 open RefreshComponent
 
-/-- The argument passed to `RefreshComponent`. -/
-structure RefreshComponentProps where
-  /-- The refresh state that is queried for updating the display. -/
-  state : WithRpcRef RefreshRef
-  deriving RpcEncodable
+/-- Displays a HTML tree, refreshing the display when the tree is updated.
 
-/-- Display an inital HTML, and repeatedly update the display with new HTML objects
-as they appear in `state`. A dedicated thread should be spawned in order to modify `state`. -/
+Use `mkRefreshComponentM` to conveniently spawn a dedicated thread
+from which you can update the display.
+
+The component resets its state when rendered with a new `props.state`.
+Conversely, reusing `props.state` preserves the client-side state.  -/
 @[widget_module]
-def RefreshComponent : Component RefreshComponentProps where
+def RefreshComponent : Component Props where
   javascript := include_str ".." /  ".." / ".lake" / "build" / "js" / "RefreshComponent.js"
-
 
 /-! ## API for creating a `RefreshComponent` -/
 
-/-- A `RefreshToken` allows you to update the state of the corresponding `RefreshComponent`
-using `RefreshToken.refresh`. -/
+/-- A `RefreshToken` allows you to manage a `RefreshComponent` instance.
+
+Use `RefreshToken.refresh` to update the HTML currently on display.
+Use `RefreshToken.cancelTk` to check if the instance has been discarded. -/
 structure RefreshToken where
-  /-- The reference that was given to the corresponding `RefreshComponent`. -/
-  refreshRef : RefreshRef
-  /-- The promise that will resolve the `next` field in `ref`.
-  If we drop the reference to this structure, and hence to this promise,
-  the `next` field will resolve to `none`. -/
-  private promise : IO.Ref (IO.Promise VersionedHtml)
+  /-- If this token is set, the `RefreshToken` is no longer displayed in the UI.
+  Subsequent `refresh` calls will have no effect.
+  We recommend passing this token to `Core.Context` if running `CoreM` computations. -/
+  cancelTk : IO.CancelToken
+  /-- The promise that `UpdatableHtml.next` waits for.
+  If we drop the `RefreshToken`, and hence this promise,
+  `UpdatableHtml.next` will resolve to `none`.
+  This ensures that no `awaitNextHtml` call is left hanging forever
+  when all threads holding onto this `RefreshToken` have exited. -/
+  private promise : IO.Ref (IO.Promise UpdatableHtml)
 
-/-- Create a fresh `RefreshToken` with initial HTML `initial`. -/
-private def RefreshToken.new (initial : Html) : BaseIO RefreshToken := do
+/-- Create a fresh `RefreshToken`. -/
+private def RefreshToken.new : BaseIO RefreshToken := do
   let promise ← IO.Promise.new
-  return {
-    refreshRef := ⟨← IO.mkRef { curr := { html := initial, idx := 0 }, next := promise.result? }⟩
-    promise := ← IO.mkRef promise }
+  return { cancelTk := ← IO.CancelToken.new, promise := ← IO.mkRef promise }
 
-/-- Update the current HTML to be `html`.
+/-- Update the current HTML tree to be `html`.
+
 This function makes use of `ST.Ref.take` in order to be thread safe.
-That is, if multiple different threads call `RefreshToken.refresh` with the same refresh token,
-a call will block other calls until it is finished. -/
+That is, if multiple threads call `refresh` with the same token,
+each thread will make its update atomically. -/
 def RefreshToken.refresh (token : RefreshToken) (html : Html) : BaseIO Unit := unsafe do
-  let { refreshRef, promise } := token
-  let idx := (← refreshRef.ref.take).curr.idx + 1
-  (← promise.get).resolve { html, idx }
+  let { promise, .. } := token
   let newPromise ← IO.Promise.new
+  let oldPromise ← promise.take
+  oldPromise.resolve { curr := html, next := newPromise.result? }
   promise.set newPromise
-  refreshRef.ref.set { curr := { html, idx }, next := newPromise.result? }
+
+/-- Create a `RefreshComponent` instance together with a token to manage it. -/
+def mkRefreshComponent (initial : Html := .text "") : BaseIO (Html × RefreshToken) := do
+  let token ← RefreshToken.new
+  let html := { curr := initial, next := (← token.promise.get).result? }
+  return (<RefreshComponent state={← WithRpcRef.mk html}
+      cancelTk={← WithRpcRef.mk token.cancelTk} />, token)
 
 variable {m} [Monad m] [MonadDrop m (EIO Exception)] [MonadLiftT BaseIO m]
 
-/-- Create an HTML, together with a `RefreshToken` that can be used to update this HTML. -/
-def mkRefreshComponent (initial : Html := .text "") : BaseIO (Html × RefreshToken) := do
-  let token ← RefreshToken.new initial
-  return (<RefreshComponent state={← WithRpcRef.mk token.refreshRef}/>, token)
-
-/-- Create a `RefreshComponent`. In order to implicitly support cancellation, `m` should extend
-`CoreM`, and hence have access to a cancel token. -/
+/-- Create a `RefreshComponent` together with a dedicated thread that drives it by running `k`. -/
 def mkRefreshComponentM (initial : Html) (k : RefreshToken → m Unit) : m Html := do
   let (html, token) ← mkRefreshComponent initial
   discard <| BaseIO.asTask (prio := .dedicated) <|
-    (← dropM <| k token).catchExceptions fun ex => token.refresh =<< do
-      if let .internal id _ := ex then
-        if id == interruptExceptionId then
-          return .text "This component was cancelled"
-      return <span>
+    (← dropM <| k token).catchExceptions fun ex => do
+      let msg :=
+        <span>
           An error occurred while refreshing this component:
           <InteractiveMessage msg={← WithRpcRef.mk ex.toMessageData}/>
         </span>
+      token.refresh msg
   return html
-
-/-- Create a `RefreshComponent`. Explicitly support cancellation by creating a cancel token,
-and setting the previous cancel token. This is useful when the component depends on the selections
-in the goal, so that after making a new selection, the previous computation is cancelled.
-
-Note: The cancel token is only set when a new computation is started.
-  When the infoview is closed, this unfortunately doesn't set the cancel token. -/
-def mkCancelRefreshComponent [MonadWithReaderOf Core.Context m]
-    (cancelTkRef : IO.Ref IO.CancelToken) (initial : Html) (k : RefreshToken → m Unit) :
-    m Html := do
-  let cancelTk ← IO.CancelToken.new
-  let oldTk ← (cancelTkRef.swap cancelTk : BaseIO _)
-  oldTk.set
-  mkRefreshComponentM initial fun token ↦
-    withTheReader Core.Context ({· with cancelTk? := cancelTk }) <| k token
-
-abbrev CancelTokenRef := IO.Ref IO.CancelToken
-
-instance : TypeName CancelTokenRef := unsafe .mk CancelTokenRef ``CancelTokenRef
-
-/-- `CancelPanelWidgetProps` are the arguments passed to a widget which supports cancellation. -/
-structure CancelPanelWidgetProps extends PanelWidgetProps where
-  /-- `cancelTkRef` is a reference to the cancel token of the most recent instance of the widget. -/
-  cancelTkRef : WithRpcRef (IO.Ref IO.CancelToken)
-  deriving RpcEncodable
-
-/-- Return a widget that supports cancellation via `CancelPanelWidgetProps`.
-The widget can be activated with for example `addPanelWidgetLocal` or  `addPanelWidgetGlobal`. -/
-def mkCancelPanelWidget (component : Component CancelPanelWidgetProps) : CoreM WidgetInstance := do
-  let cancelTkRef ← WithRpcRef.mk (← IO.mkRef (← IO.CancelToken.new))
-  Widget.WidgetInstance.ofHash component.javascriptHash
-    (return json% {cancelTkRef : $(← rpcEncode cancelTkRef)})
 
 end ProofWidgets
--- widget/src/RefreshComponent.tsx
+++ widget/src/RefreshComponent.tsx
@@ -3,58 +3,50 @@ Copyright (c) 2025 Jovan Gerbscheid. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Jovan Gerbscheid
 */
-import React from "react";
-import { useRpcSession, RpcPtr } from "@leanprover/infoview";
-import HtmlDisplay, { Html } from './htmlDisplay';
+import React from 'react'
+import { useRpcSession, RpcPtr } from '@leanprover/infoview'
+import HtmlDisplay, { Html } from './htmlDisplay'
 
-/** The arguments passed to a `RefreshComponent` */
 interface RefreshComponentProps {
-  /** The ref to the refresh state of the HTML display */
-  state : RpcPtr<'RefreshRef'>
-}
-
-interface AwaitRefreshParams {
-  state : RpcPtr<'RefreshRef'>
-  oldIdx : number
-}
-
-interface VersionedHtml {
-  /** The new HTML to display */
-  html : Html
-  idx : number
+  state : RpcPtr<'UpdatableHtml'>
+  cancelTk : RpcPtr<'IO.CancelToken'>
 }
 
 export default function RefreshComponent(props: RefreshComponentProps): JSX.Element {
   const rs = useRpcSession()
   const [html, setHtml] = React.useState<Html | null>(null)
+  const st = React.useRef<RpcPtr<'UpdatableHtml'>>(props.state)
 
-  // Repeatedly call Lean to update
   React.useEffect(() => {
     let cancelled = false
-    async function loop(idx: number) {
-        const result = await rs.call<AwaitRefreshParams, VersionedHtml | null>(
-            "ProofWidgets.RefreshComponent.awaitRefresh", { oldIdx: idx, state: props.state })
-        if (cancelled || !result) return
-        setHtml(result.html)
-        return loop(result.idx)
+    async function loop() {
+      const result = await rs.call<RpcPtr<'UpdatableHtml'>, [Html, RpcPtr<'UpdatableHtml'>] | null>(
+        'ProofWidgets.RefreshComponent.awaitNextHtml', st.current)
+      if (cancelled || !result) return
+      st.current = result[1]
+      setHtml(result[0])
+      return loop()
     }
+    const ac = new AbortController()
+    rs.call<RefreshComponentProps, null>('ProofWidgets.RefreshComponent.monitor', props,
+      { autoCancel: true, abortSignal: ac.signal });
     (async () => {
-        // Set the HTML to the current value at the start of each re-render
-        const result = await rs.call<RpcPtr<'RefreshRef'>, VersionedHtml>(
-            "ProofWidgets.RefreshComponent.getCurrState", props.state)
-        if (cancelled) return
-        setHtml(result.html)
-        // And then repeatedly refresh the HTML
-        return loop(result.idx)
+      // Display the HTML tree provided in the initial props
+      const result = await rs.call<RpcPtr<'UpdatableHtml'>, Html>(
+        'ProofWidgets.RefreshComponent.getCurrHtml', props.state)
+      if (cancelled) return
+      st.current = props.state
+      setHtml(result)
+      // Then repeatedly await updates to the display
+      return loop()
     })()
     return () => {
-        cancelled = true
+      cancelled = true
+      ac.abort()
     }
-  // React checks if `props` has changed using pointer equality.
-  // We claim that here this is equivalent to deep equality of the objects.
-  // This is because whenever a new `props` is created, this is done by Lean
-  // in a way that creates fresh `RpcPtr`s.
-  }, [props])
+  // Use the RPC reference ID as the only dep.
+  // This gives Lean code control over when this component is reset.
+  }, [RpcPtr.toKey(props.state)])
   // For the short time that `html` doesn't have a value yet, we return the empty HTML.
   return html ? <HtmlDisplay html={html}/> : <></>
 }
